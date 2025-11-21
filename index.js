93a62e5413acee28cfe895c8c334cab867894995
SHUBAM LUNAWAT <shubamlunawat15@gmail.com>
2025-11-21 23:07:34 +0530

    Updating files

--- index.js
+++ index.js
@@ -1,8 +1,2 @@
-import { createRequire } from 'module';
-const require = createRequire(import.meta.url);
-
-// Import the ES module app
-const appModule = await import('./src/index.js');
-const app = appModule.default;
-
-module.exports = app;
+import app from './src/index.js';
+export default app;
